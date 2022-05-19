003ff4e77c442f0c1f0434d911b868886a330556
IvanBM18 <ivan.barba7476@alumnos.udg.mx>
2022-05-18 20:54:49 -0500

    Still Fixig

--- app/models/medicine.py
+++ app/models/medicine.py
@@ -3,13 +3,13 @@ from typing import Dict
 
 
 class Medicine:
-  def __init__(self, id:int = None, concept:str = None, creation_date:date = None, stock:int=None, price:float=None,  user_id:int=None, expiration_date:date=None, location:str=None):
+  def __init__(self, id:int = None, concept:str = None, creation_date:date = None, stock:int=None, price:float=None, typeM:str=None, user_id:int=None, expiration_date:date=None, location:str=None):
     self.id = id
     self.concept = concept
     self.creation_date = creation_date
     self.stock = stock
     self.price = price
-    self.type = type
+    self.typeM = typeM
     self.location = location
     self.user_id = user_id
     self.expiration_date= expiration_date
@@ -22,7 +22,7 @@ class Medicine:
       creation_date= json.get('creation_date'),
       stock= json.get('stock'),
       price= json.get('price'),
-      type= json.get('type'),
+      typeM= json.get('type'),
       user_id= json.get('user_id'),
       expiration_date= json.get('expiration_date'),
     )
